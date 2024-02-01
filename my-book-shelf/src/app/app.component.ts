081322af76fb6da4bff150f2e5b22a8d4ed38f7e
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-01 08:32:36 +0300

    feat: add layout and sidebar

--- my-book-shelf/src/app/app.component.ts
+++ my-book-shelf/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './core/pages/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ my-book-shelf/src/app/core/pages/layout/layout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
