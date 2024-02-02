e123193316c989735b65b94bdd19b18a2ee31263
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-02 04:39:27 +0300

    feat: add header

--- my-book-shelf/src/app/core/components/header/header.component.ts
+++ my-book-shelf/src/app/core/components/header/header.component.ts
@@ -1,12 +1,12 @@
+import { SettingsComponent } from '../settings/settings.component';
+import { SearchBarComponent } from './../search-bar/search-bar.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent, SettingsComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
