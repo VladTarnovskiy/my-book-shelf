decbe324159a0f5ee6d4dc62093250e490141abd
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-02 06:37:56 +0300

    feat: add home books

---
+++ my-book-shelf/src/app/home/components/home-book/home-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-book.component.html',
+  styleUrl: './home-book.component.scss'
+})
+export class HomeBookComponent {
+
+}
--- my-book-shelf/src/app/home/pages/home/home.component.ts
+++ my-book-shelf/src/app/home/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { QuoteComponent } from '../../components/quote/quote.component';
+import { HomeBookComponent } from '../../components/home-book/home-book.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [QuoteComponent],
+  imports: [QuoteComponent, HomeBookComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
