0c22d2a517a6ef0c3de115c95ab403f924783d58
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-08 14:22:34 +0300

    feat: add profile and lang menus

--- my-book-shelf/src/app/core/components/settings/settings.component.ts
+++ my-book-shelf/src/app/core/components/settings/settings.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfileMenuComponent } from '../profile-menu/profile-menu.component';
+import { LangMenuComponent } from '../lang-menu/lang-menu.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ProfileMenuComponent, LangMenuComponent],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
