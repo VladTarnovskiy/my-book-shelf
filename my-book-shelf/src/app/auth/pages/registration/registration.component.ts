09c222dd98100fea9e4a678074ef06e91273c5c1
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-12 00:41:53 +0300

    feat: add registration page

--- my-book-shelf/src/app/auth/pages/registration/registration.component.ts
+++ my-book-shelf/src/app/auth/pages/registration/registration.component.ts
@@ -1,36 +1,74 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
-  FormBuilder,
+  FormControl,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { ValidateName } from '../../validators/name';
 import { ValidatePassword } from '../../validators/password';
-import { IUserDetails } from '../../models/user';
 import { CommonModule } from '@angular/common';
+import { IUserDetailsRegistrationForm } from '../../models/user';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, RouterModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegistrationComponent {
-  registerForm = this.fb.group({
-    name: ['', [Validators.required, Validators.maxLength(40), ValidateName()]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, ValidatePassword()]],
+  constructor(private authService: AuthService) {}
+
+  registerForm = new FormGroup<IUserDetailsRegistrationForm>({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(40),
+        ValidateName(),
+      ],
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, ValidatePassword()],
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, ValidatePassword()],
+    }),
   });
 
-  constructor(private fb: FormBuilder) {}
-
   onSubmit() {
-    const formUserData = this.registerForm.getRawValue() as IUserDetails;
-    if (this.registerForm.status === 'VALID') {
-      console.log(formUserData);
-    }
+    const formUserData = this.registerForm.getRawValue();
+    console.log(formUserData);
+    console.log(this.registerForm.status);
+    this.authService.signUp({
+      email: formUserData.email,
+      password: formUserData.password,
+    });
+    // if (this.registerForm.status === 'VALID') {
+    //   console.log(formUserData);
+    // }
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
   }
 }
