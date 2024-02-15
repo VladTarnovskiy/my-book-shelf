5e50aa035fc785e2ec2aa98cc140449ee8310fa1
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-15 17:17:25 +0300

    refactor: split book response interface

--- my-book-shelf/src/app/core/interfaces/booksResp.ts
+++ my-book-shelf/src/app/core/interfaces/booksResp.ts
@@ -9,89 +9,101 @@ export interface IBookResp {
   id: string;
   etag: string;
   selfLink: string;
-  volumeInfo: {
-    title: string;
-    subtitle: string;
-    authors: string[];
-    publisher: string;
-    publishedDate: string;
-    description: string;
-    industryIdentifiers: [
-      {
-        type: string;
-        identifier: string;
-      },
-    ];
-    readingModes: {
-      text: boolean;
-      image: boolean;
-    };
-    pageCount: number;
-    printType: string;
-    categories: string[];
-    maturityRating: string;
-    allowAnonLogging: boolean;
-    contentVersion: string;
-    panelizationSummary: {
-      containsEpubBubbles: boolean;
-      containsImageBubbles: boolean;
-    };
-    imageLinks: {
-      smallThumbnail: string;
-      thumbnail: string;
-    };
-    language: string;
-    previewLink: string;
-    infoLink: string;
-    canonicalVolumeLink: string;
-  };
-  saleInfo: {
-    country: string;
-    saleability: string;
-    isEbook: boolean;
-    listPrice: {
-      amount: number;
-      currencyCode: string;
-    };
-    retailPrice: {
-      amount: number;
-      currencyCode: string;
-    };
-    buyLink: string;
-    // offers: [
-    //   {
-    //     finskyOfferType: 1;
-    //     listPrice: {
-    //       amountInMicros: 21990000;
-    //       currencyCode: 'USD';
-    //     };
-    //     retailPrice: {
-    //       amountInMicros: 21990000;
-    //       currencyCode: 'USD';
-    //     };
-    //     giftable: boolean;
-    //   },
-    // ];
-  };
-  accessInfo: {
-    country: string;
-    viewability: string;
-    embeddable: boolean;
-    publicDomain: boolean;
-    textToSpeechPermission: string;
-    epub: {
-      isAvailable: boolean;
-      acsTokenLink: string;
-    };
-    pdf: {
-      isAvailable: boolean;
-      acsTokenLink: string;
-    };
-    webReaderLink: string;
-    accessViewStatus: string;
-    quoteSharingAllowed: boolean;
-  };
+  volumeInfo: VolumeInfo;
+  saleInfo: SalesInfo;
+  accessInfo: AccessInfo;
   searchInfo: {
     textSnippet: string;
   };
+}
+
+interface VolumeInfo {
+  title: string;
+  subtitle: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: string;
+  description: string;
+  industryIdentifiers: IndustryIdentifiers[];
+  readingModes: ReadingModes;
+  pageCount: number;
+  printType: string;
+  categories: string[];
+  maturityRating: string;
+  allowAnonLogging: boolean;
+  contentVersion: string;
+  panelizationSummary: PanelizationSummary;
+  imageLinks: ImageLinks;
+  language: string;
+  previewLink: string;
+  infoLink: string;
+  canonicalVolumeLink: string;
+}
+
+interface IndustryIdentifiers {
+  type: string;
+  identifier: string;
+}
+
+interface ReadingModes {
+  text: boolean;
+  image: boolean;
+}
+
+interface PanelizationSummary {
+  containsEpubBubbles: boolean;
+  containsImageBubbles: boolean;
+}
+
+interface ImageLinks {
+  smallThumbnail: string;
+  thumbnail: string;
+}
+
+interface SalesInfo {
+  country: string;
+  saleability: string;
+  isEbook: boolean;
+  listPrice: {
+    amount: number;
+    currencyCode: string;
+  };
+  retailPrice: {
+    amount: number;
+    currencyCode: string;
+  };
+  buyLink: string;
+  offers: Offers[];
+}
+
+interface Offers {
+  finskyOfferType: number;
+  listPrice: {
+    amountInMicros: number;
+    currencyCode: string;
+  };
+  retailPrice: {
+    amountInMicros: number;
+    currencyCode: string;
+  };
+  giftable: boolean;
+}
+
+interface AccessInfo {
+  country: string;
+  viewability: string;
+  embeddable: boolean;
+  publicDomain: boolean;
+  textToSpeechPermission: string;
+  epub: {
+    isAvailable: boolean;
+    acsTokenLink: string;
+  };
+  pdf: {
+    isAvailable: boolean;
+    acsTokenLink: string;
+  };
+  webReaderLink: string;
+  accessViewStatus: string;
+  quoteSharingAllowed: boolean;
 }
