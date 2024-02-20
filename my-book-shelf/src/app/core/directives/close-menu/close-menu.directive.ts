449e17ff937a55c293f2d3344813e1271ee19e53
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-20 14:37:07 +0300

    feat: add close menu directive

---
+++ my-book-shelf/src/app/core/directives/close-menu/close-menu.directive.ts
@@ -0,0 +1,19 @@
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appCloseMenu]',
+  standalone: true,
+})
+export class CloseMenuDirective {
+  @Output() closeMenu = new EventEmitter<boolean>(false);
+
+  @HostListener('blur') onMouseEnter() {
+    this.closeMenu.emit(false);
+  }
+}
