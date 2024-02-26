36023fadf07f15a1a9f9e3589308c6de4ea70aad
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-26 19:33:42 +0300

    feat: add translate function

--- my-book-shelf/src/app/app.component.ts
+++ my-book-shelf/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToasterContainerComponent } from './core/components/toaster-container/toaster-container.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import englishLang from '../assets/i18n/en.json';
+import russianLang from '../assets/i18n/ru.json';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,13 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
   constructor(translate: TranslateService) {
+    translate.setTranslation('en', englishLang);
+    translate.setTranslation('ru', russianLang);
     translate.setDefaultLang('en');
     translate.use('en');
   }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
     // alert(
     //   'The Google books API is not available in Belarus, please, use a VPN to make the service work correctly!'
--- my-book-shelf/src/app/core/components/lang-menu/lang-menu.component.ts
+++ my-book-shelf/src/app/core/components/lang-menu/lang-menu.component.ts
@@ -1,18 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  LangChangeEvent,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lang-menu',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './lang-menu.component.html',
   styleUrl: './lang-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LangMenuComponent {
+export class LangMenuComponent implements OnInit {
   isMenu = false;
+  activeLang = 'en';
 
   constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.activeLang = event.lang;
+    });
+  }
 
   onLangToggle(): void {
     this.isMenu = !this.isMenu;
