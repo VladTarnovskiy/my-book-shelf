7248f772077a6d63570421d60a52a9ffdead18c0
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-02-28 07:40:33 +0300

    feat: add time of day

--- my-book-shelf/src/app/home/pages/home/home.component.ts
+++ my-book-shelf/src/app/home/pages/home/home.component.ts
@@ -44,4 +44,21 @@ export class HomeComponent implements OnInit {
     const genSearchValue = recommendedGenerator();
     this.recommendedBooksFacade.fetchRecommendedBooks(genSearchValue);
   }
+
+  getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (6 <= hours && hours < 12) {
+      timeOfDay = 'MORNING';
+    } else if (12 <= hours && hours < 18) {
+      timeOfDay = 'AFTERNOON';
+    } else if (18 <= hours && hours <= 24) {
+      timeOfDay = 'EVENING';
+    } else if (0 <= hours && hours < 6) {
+      timeOfDay = 'NIGHT';
+    }
+    return timeOfDay;
+  }
 }
