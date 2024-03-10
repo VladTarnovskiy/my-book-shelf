3ed1b08ce488ba58c959e5d7074bab97e1c49e5f
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-03-11 00:59:53 +0300

    refactor: translate not found page

--- my-book-shelf/src/app/core/pages/not-found/not-found.component.ts
+++ my-book-shelf/src/app/core/pages/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
